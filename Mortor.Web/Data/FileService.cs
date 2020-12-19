ffd49bcf1f90e077ff19f7bf4b49ff21a69501f4
Raf Ceuls <rceuls@gmail.com>
2020-12-19 16:57:04 +0100

    added delete support

--- Mortor.Web/Data/FileService.cs
+++ Mortor.Web/Data/FileService.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Mortor.Web.Data
@@ -13,6 +15,24 @@ namespace Mortor.Web.Data
             uploadPathOnFs = Path.Combine(uploadPathOnFs, imageName + ".png");
             await File.WriteAllBytesAsync(uploadPathOnFs, imageContent);
             return Path.Combine(path, imageName + ".png");
+        }
+
+        public void RemoveAllImagesFromReport(string reportId)
+        {
+            var targetPathOnFs = Path.Combine("wwwroot", "upload", reportId);
+            if (Directory.Exists(targetPathOnFs))
+            {
+                Directory.Delete(targetPathOnFs, true);
+            }
+        }
+
+        public void DeleteImage(string imagePath)
+        {
+            var targetPathOnFs = Path.Combine("wwwroot", imagePath);
+            if (File.Exists(targetPathOnFs))
+            {
+                File.Delete(targetPathOnFs);
+            }
         }
     }
 }
